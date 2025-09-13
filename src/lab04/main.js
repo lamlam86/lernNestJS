e0881141f4183f851c8fc40e758386313bf3701f
lamlam86 <bb10a2.nmlam@gmail.com>
2025-09-14 00:10:15 +0700

    test

---
+++ src/lab04/main.js
@@ -0,0 +1,14 @@
+let user = document.getElementById("username")
+let pass = document.getElementById("pass")
+let bt = document.getElementById("bt1")
+console.log(user,pass)
+let email =  "bb10a2.nmlam@gmail.com"
+let passw = "123"
+let a = ""
+let b = ""
+function click()
+{
+    a = user.value
+    b = pass.value
+   console.log(a,b)
+}
